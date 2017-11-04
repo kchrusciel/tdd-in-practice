1e7ee4242c34528c3be554644db21f8c65d6b87d
kchrusciel <krzysztof.chrusciel@outlook.com>
2017-11-04 09:56:00 +0100

    Add email producer

---
+++ email-producer/src/main/java/pl/spreadit/EmailProducerApplication.java
@@ -0,0 +1,12 @@
+package pl.spreadit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailProducerApplication.class, args);
+	}
+}
